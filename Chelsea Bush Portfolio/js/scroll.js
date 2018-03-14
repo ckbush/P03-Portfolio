4bd44ed46d1dd4a1c45ac860cec010e028f191c6
Unknown <cbush04@gmail.com>
2018-03-14 04:52:15 -0700

    Add current portfolio progress
    styleguide, page templates, about paragraph

---
+++ Chelsea Bush Portfolio/js/scroll.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y >220) {
+    $('.box-container').fadeOut(220);
+  } else {
+    $('.box-container').fadeIn(220);
+  }
+});
+
+
+$(window).scroll(function(){
+  if ($(window).width() > 60) {
+    $(".work-section").css("opacity", 3 - $(window).scrollTop() / 380);
+  }
+  });
